3babb43ff5ea7bdee4996ca5046e125137d60e65
chriskolb <39267431+chriskolb@users.noreply.github.com>
2021-07-29 17:55:56 +0200

    Update plots-single-species.R

--- single-species-models/plots-single-species.R
+++ single-species-models/plots-single-species.R
@@ -35,7 +35,7 @@ library(devtools)
 pacman::p_load(Matrix, dplyr, keras, mgcv, reticulate, tensorflow, tfprobability, 
                Metrics, DescTools, MLmetrics, caret, xgboost, recipes, yardstick
                ,ParBayesianOptimization, doParallel, muStat, scoring, blockCV, checkmate, 
-               furrr, tmap, rgeos, rstudioapi
+               furrr, tmap, rgeos, rgdal, rstudioapi
                )
 
 # force conda environment
