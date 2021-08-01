ea463e421d5a10a486df305a43f26e623529b171
chriskolb <39267431+chriskolb@users.noreply.github.com>
2021-08-01 10:08:41 +0200

    Add files via upload

---
+++ single-species-models/repo/mastergrids/R/data.R
@@ -0,0 +1,6 @@
+#' Some predifined folder paths in the MAP mastergrids folder
+#'
+#' A collection of mastergrid paths that can be used to narrow the search path
+#' of \code{search_files}.
+#'
+"mastergrid_paths"
