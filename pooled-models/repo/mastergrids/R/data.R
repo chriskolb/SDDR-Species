ae7f4291b07cc8b8cf8bf5b65f277b0fe71f60e3
chriskolb <39267431+chriskolb@users.noreply.github.com>
2022-06-14 20:58:44 +0200

    Delete pooled-models/repo/mastergrids directory

--- pooled-models/repo/mastergrids/R/data.R
+++
@@ -1,6 +0,0 @@
-#' Some predifined folder paths in the MAP mastergrids folder
-#'
-#' A collection of mastergrid paths that can be used to narrow the search path
-#' of \code{search_files}.
-#'
-"mastergrid_paths"
